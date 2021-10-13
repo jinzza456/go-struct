57905a03653b73deddd579e4030efd75c553a1ca
jinzza456 <jinzza456@gmail.com>
2021-10-13 13:48:23 +0900

    struct method

--- struct.go
+++ struct.go
@@ -7,17 +7,17 @@ type Person struct {
 	age  int
 }
 
+func (Person) PrintName() { //
+	fmt.Print(p.name)
+}
+
 func main() {
 	var p Person
-	p1 := Person{"개똥이", 15}
-	p2 := Person{name: "소똥이", age: 21}
-	p3 := Person{name: "Carson"}
-	p4 := Person{}
-
-	fmt.Println(p, p1, p2, p3, p4)
 
 	p.name = "Smith"
 	p.age = 24
 
 	fmt.Println(p)
+
+	p.PrintName()
 }
